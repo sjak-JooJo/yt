f39e726f314441e2dce2ad3f81acc798c93db696
JooJo <sjak0101@gmail.com>
2022-04-27 09:47:15 +0100

    secondt commit

--- js/main.js
+++ js/main.js
@@ -2,4 +2,7 @@ $(window).scroll(function(){
     if($(window).scrollTop() > 0){
         $('.navbar').css('background', '#212529');
     }
+    else{
+        $('.navbar').css('background', 'transparent');
+    }
 });
